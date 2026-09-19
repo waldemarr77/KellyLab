4ab15bd6f921c303709af00f6a2b3613508a3de2
Vlad <bahlaiv170307@gmail.com>
2026-09-19 23:13:48 +0300

    feat: admin for users

--- users/admin.py
+++ users/admin.py
@@ -1,2 +1,49 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = [
+        'username', 
+        'email', 
+        'target_position', 
+        'experience_level',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'last_login',
+    ]
+
+    list_filter = [
+        'experience_level',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+    ]
+
+    search_fields = [
+        'username', 
+        'email', 
+        'target_position',
+    ]
+    
+    readonly_fields = ['date_joined', 'last_login',]
+    ordering = ['username',]
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Підготовка до співбесіди', {
+            'fields': ('target_position', 'experience_level'),
+        }),
+        ('Аватар', {
+            'fields': ('avatar',),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Додаткова інформація', {
+            'fields': ('email', 'target_position', 'experience_level'),
+        }),
+    )
